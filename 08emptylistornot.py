b3c6d129bb5fba51d1b6b4a2a3370b432bfb708c
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2023-01-06 17:32:26 +0530

    Create 08emptylistornot.py

---
+++ 08emptylistornot.py
@@ -0,0 +1,11 @@
+# Write a Python program to check a list is empty or not. 
+aList = [1,2,3,4,4,5,6,6,7,8,9]
+sList = []
+if len(aList) > 0:
+    print(True)
+else:
+    print(False)
+if len(sList) > 0:
+    print(True)
+else:
+    print(False)
